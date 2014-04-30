e7b00605384c9742e7ffc2645705a00847eb2cae
jlgigante <jlg@jlgigante.fr>
2014-04-30 16:43:55 +0200

    retour arrière sur le wording du titre vestiaire lol

--- content.php
+++ content.php
@@ -45,7 +45,7 @@ $code_content = array(
 
 
 $vestiaire_content = array(
-	'titre' => 'Le Vestiaire de l\'homme Armani Code',
+	'titre' => 'Le Vestiaire',// de l\'homme Armani Code',
 	'articles' => array (
 					array(
 						'id'		=>	1,
